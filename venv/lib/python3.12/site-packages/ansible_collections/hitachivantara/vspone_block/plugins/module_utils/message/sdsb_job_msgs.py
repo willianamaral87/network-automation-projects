316dd7345490f86fd3ff8c80a93ab59b7fbda860
Williuan Amaral <willian.amaral.ti@email.com>
2026-03-02 18:33:09 -0300

    Fix Project 1

--- venv/lib/python3.12/site-packages/ansible_collections/hitachivantara/vspone_block/plugins/module_utils/message/sdsb_job_msgs.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class SDSBJobValidationMsg(Enum):
-
-    INVALID_COUNT = (
-        "Invalid count information provided. It must be in the range 1 to 100."
-    )
--- venv/lib/python3.12/site-packages/ansible_collections/hitachivantara/vspone_block/plugins/module_utils/message/vsp_storage_system_monitor_msgs.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-
-
-class VSPStotageSystemMonitorValidateMsg(Enum):
-    ALERT_TYPE_NEEDED = "alert_type is required when query field is alerts."
